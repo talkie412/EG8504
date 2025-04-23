52efc4031a7116901127be53125e4c8f3b2295bd
talkie412 <talkie412@gmail.com>
2025-04-23 23:25:05 +0900

    Add image header file and implement image data handling in main loop

---
+++ EG8504_Arduino/include/image.h
@@ -0,0 +1,9 @@
+#define IMAGE_H
+#define Image_H
+
+#include <avr/pgmspace.h>
+
+// 外部リンケージで宣言
+extern const uint8_t data[] PROGMEM;
+
+#endif
--- EG8504_Arduino/src/main.cpp
+++ EG8504_Arduino/src/main.cpp
@@ -1,21 +1,37 @@
 #include <Arduino.h>
+#include <avr/pgmspace.h>
+#include "image.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define LCD_height 240
+#define LCD_width 80
+
+extern const uint8_t data[];
 
 void setup()
 {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
+  DDRB |= B00001111;
+  DDRC |= B00000111;
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y)
-{
-  return x + y;
+  for (int i = 0; i < LCD_height; i++)
+  {
+    for (int j = 0; j < LCD_width; j++)
+    {
+      uint8_t value = pgm_read_byte(&data[j + i * LCD_width]); // PROGMEM から読み込み
+      PORTB = value;
+      PORTC |= B00000001; // XSCL HIGH
+      PORTC &= B11111110; // XSCL LOW
+    }
+    if (i == 0)
+      PORTC |= B00000100; // DIN HIGH
+    PORTC |= B00000010;   // LP HIGH
+    PORTC &= B11111101;   // LP LOW
+    if (i == 0)
+      PORTC &= B11111011; // DIN LOW
+    delayMicroseconds(10);
+  }
 }
